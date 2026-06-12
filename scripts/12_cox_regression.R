1e0f7382ae0437673edf0e2453fec72a0308dfb9
vortexa <vaishnavimangam579@gmail.com>
2026-06-12 15:34:19 +0530

    Update 12_cox_regression.R

--- scripts/12_cox_regression.R
+++ scripts/12_cox_regression.R
@@ -1,3 +1,55 @@
-# Script:12_cox_regression.R
+#=========================================================
+# Script: 12_cox_regression.R
+# Author: Vaishnavi Mangam
 # Project: TCGA-LUAD-lncRNA-Analysis
-# Author: Vaishnavi Mangam 
+#
+# Description:
+# Performs univariate Cox proportional hazards regression
+# for AC135012.3 expression using TCGA-LUAD survival data.
+#
+# Input:
+# TCGA_LUAD_SURVIVAL_RESULTS.RData
+#
+# Output:
+# TCGA_LUAD_COX_RESULTS.RData
+# Cox_regression_summary.txt
+#=========================================================
+
+library(survival)
+
+#---------------------------------------------------------
+# Load survival data
+#---------------------------------------------------------
+
+load("TCGA_LUAD_SURVIVAL_RESULTS.RData")
+
+#---------------------------------------------------------
+# Univariate Cox regression
+#---------------------------------------------------------
+
+cox2 <- coxph(
+  Surv(OS_time, status) ~ expression,
+  data = surv_data2
+)
+
+cox_summary <- summary(cox2)
+
+cox_summary
+
+#---------------------------------------------------------
+# Save Cox summary as text
+#---------------------------------------------------------
+
+sink("Cox_regression_summary.txt")
+print(cox_summary)
+sink()
+
+#---------------------------------------------------------
+# Save R objects
+#---------------------------------------------------------
+
+save(
+  cox2,
+  cox_summary,
+  file = "TCGA_LUAD_COX_RESULTS.RData"
+)
